856756d127eb023301276b32efed12de3075ae2f
Mark Smagin <lifeindeadcity@gmail.com>
2022-05-20 02:18:52 +0300

    NewGen Vision тестовое задание v2

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ button.addEventListener("click", function() {
     let filtered =  courses.filter(function(value) {
         return filterRange(value.prices, leftRange, rightRange);
     });
+
+    sortResult(filtered);
     
     result.classList.remove("hidden");
 
@@ -37,18 +39,32 @@ button.addEventListener("click", function() {
         newString.innerHTML = `${filtered[i].name}`;
         newString.classList.add("line");
         result.appendChild(newString);
-        console.log(filtered[i].prices);
     }
 })
 
+// Функция фильтрации списка курсов по заданному диапазону цен
 function filterRange(arr, a, b) {
+    // Проверка на наличие левой и правой границ диапазона, которые задает пользователь, если их нет, то выдается весь список курсов
     if (a === null && b === null){
         return arr
-    } else if (a === null){
+    }
+    // Проверка на наличие левой границы диапозона, если такой нет, то будет выдан список курсов, у которых самая низкая цена меньше, чем правая граница, которую задал пользователь
+    else if (a === null){
         return arr[0] <= b;
-    } else if (b === null){
-        return a <= arr[0] || a <= arr[1] || arr[1] === null;
-    } else {
+    } 
+    // Проверка на наличие правой границы диапозона, если такой нет, то будет выдан список курсов, у которых самая высокая цена больше, чем левая граница, которую задал пользователь,
+    // либо если самая высокая цена у курса не задана
+    else if (b === null){
+        return a <= arr[1] || arr[1] === null;
+    } 
+    // Во всех остальных случаях будут выведены все курсы, у которых самая высокая цена больше левой границы заданного диапозона или, если самая высокая цена не указана, и, если самая низкая цена
+    // курса меньше, чем правая граница заданного диапазона, или она не указана
+    else {
         return (a <= arr[1] || arr[1] === null) && (arr[0] <= b || arr[0] === null);
     }
+}
+
+// Функция сортироки по возрастанию цены
+function sortResult(arr) {
+    arr.sort((a, b) => a.prices[0] > b.prices[0] ? 1 : -1);
 }
